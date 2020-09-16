7d73a4bc2349326e2210b694724cfb158878ce55
Anderson Campos <andersonniki@hotmail.com>
2020-09-16 18:22:21 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,61 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: '5',
+        candidatos: [
+            {
+                numero: '31811',
+                nome: 'Anderson Campos',
+                partido: 'PMDB',
+                fotos: [
+                    {
+                        url:'anderson.jpg',legenda: 'Vereador'
+                    }
+                ]
+            },
+            {
+                numero: '31761',
+                nome: 'Maiara Santa Rosa',
+                partido: 'PCdoB',
+                fotos: [
+                    {
+                        url:'maiara.jpg',legenda: 'Vereadora'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: '2',
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Anderson Campos',
+                partido: 'PT',
+                fotos: [
+                    {
+                        url:'anderson.jpg',legenda: 'Prefeito'
+                    },
+                    {
+                        url:'maiara.jpg', legenda: 'Vice-prefeito', small: true
+                    }
+                ]
+            },
+            {
+                numero: '88',
+                nome: 'Maiara Santa Rosa',
+                partido: 'PP',
+                fotos: [
+                    {
+                        url:'maiara.jpg',legenda: 'Prefeita'
+                    },
+                    {
+                        url:'anderson.jpg', legenda: 'Vice-prefeito', small: true
+                    }
+                ]
+            }
+        ]
+    }
+
+];
